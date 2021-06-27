de8ea669ba584a4a0eb6ca34d1143f9ba2e74956
andrew quenette <and1roxx43@gmail.com>
2021-06-27 21:57:48 +1000

    Modified css, added comment.js, single-post.handlebars

---
+++ public/script/comment.js
@@ -0,0 +1,28 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_area = document.querySelector(".comment-area").value.trim();
+
+    const post_id = window.location.toString().split("/")[ window.location.toString().split("/"). length - 1];
+
+    if (comment_area) {
+        const response = await fetch("/api/comments", {
+            method: "POST",
+            body: JSON.stringify({
+                post_id,
+                comment_area
+
+            }),
+            headers: { "Content-Type" : "application/json"}
+        
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector("comment-form").addEventListener("submit", commentFormHandler);
